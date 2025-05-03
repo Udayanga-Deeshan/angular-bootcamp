4c077e897f26197e4122719f389ed6065aea77fd
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2025-05-03 19:59:54 +0530

    two way data binding

--- src/app/pages/data-binding/data-binding.component.ts
+++ src/app/pages/data-binding/data-binding.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.component.html',
   styleUrl: './data-binding.component.css'
 })
@@ -11,7 +12,7 @@ export class DataBindingComponent {
   name:string="Udayaga"
   designation:string="Software engineer"
   imageUrl:string="https://img.freepik.com/free-vector/colourful-illustration-programmer-working_23-2148281410.jpg?t=st=1746258250~exp=1746261850~hmac=a60f0a91238cbb0f15fa2ebffc46ac1c3e0b290be46958f8f328ad09d87a9126&w=740"
-
+  random=""
   saveData(){
     alert("Data saved Successfully")
   }
