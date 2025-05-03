3b5cf24ba5a0a50605f921bc3820fd8ec2127205
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2025-05-03 13:19:44 +0530

    property binding

--- src/app/pages/data-binding/data-binding.component.ts
+++ src/app/pages/data-binding/data-binding.component.ts
@@ -10,4 +10,5 @@ export class DataBindingComponent {
 
   name:string="Udayaga"
   designation:string="Software engineer"
+  imageUrl:string="https://img.freepik.com/free-vector/colourful-illustration-programmer-working_23-2148281410.jpg?t=st=1746258250~exp=1746261850~hmac=a60f0a91238cbb0f15fa2ebffc46ac1c3e0b290be46958f8f328ad09d87a9126&w=740"
 }
