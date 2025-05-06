2bb33f97bd6bb6a5bb01ec9c540b9ffb03f2fdab
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2025-05-06 13:20:35 +0530

    life cycle hooks

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import {  Router } from '@angular/router';
 
 @Component({
@@ -7,9 +7,48 @@ import {  Router } from '@angular/router';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent implements OnChanges,OnInit,DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy {
 
   constructor(private router:Router){}
+
+  ngOnChanges(changes: SimpleChanges): void {
+      console.log("1.Onchanges method called");
+      
+  }
+
+  ngOnInit(): void {
+    // use for API call
+      console.log("2. ng OnInit() called");
+      
+  }
+
+  ngDoCheck(): void {
+      // use for every change detection
+      console.log("Ng Dochecked() called");
+      
+  }
+
+  ngAfterContentInit(): void {
+    // sue for ng content
+      console.log("ng AfterContentInit called...");
+      
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("ng AfterContentChecked called...");
+      
+  }
+  ngAfterViewInit(): void {
+    // sue for view child
+    console.log("ng AfterViewInit called...");
+  }
+  ngAfterViewChecked(): void {
+    console.log("ng AfterViewChecked called...");
+  }
+
+  ngOnDestroy(): void {
+    console.log("ng OnDestroy called...");
+  }
 
   Login(){
       this.router.navigateByUrl("data-binding")
