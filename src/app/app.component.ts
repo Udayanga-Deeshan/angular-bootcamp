09e9a7daf25e441d5aceda86b442e705667bef7f
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2025-05-06 12:00:45 +0530

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { UserComponent } from "./pages/user/user.component";
 import { DataBindingComponent } from "./pages/data-binding/data-binding.component";
 import { DirectivesComponent } from "./pages/directives/directives.component";
 import { NgswitchUsecasesComponent } from "./pages/ngswitch-usecases/ngswitch-usecases.component";
 import { AttributeDirectivesComponent } from "./pages/attribute-directives/attribute-directives.component";
 import { SignalsComponent } from "./pages/signals/signals.component";
+import { NavbarComponent } from "./common/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserComponent, DataBindingComponent, DirectivesComponent, NgswitchUsecasesComponent, AttributeDirectivesComponent, SignalsComponent],
+  imports: [RouterOutlet, UserComponent, DataBindingComponent, DirectivesComponent, NgswitchUsecasesComponent, AttributeDirectivesComponent, SignalsComponent, NavbarComponent,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { DataBindingComponent } from './pages/data-binding/data-binding.component';
+import { DirectivesComponent } from './pages/directives/directives.component';
+import { NgswitchUsecasesComponent } from './pages/ngswitch-usecases/ngswitch-usecases.component';
+import { PageNotFoundComponent } from './common/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:NavbarComponent},
+    {path:"data-binding", component:DataBindingComponent},
+    {path:"structural-directives", component:DirectivesComponent},
+    {path:"ng-switch", component:NgswitchUsecasesComponent},
+    {path:'**',component:PageNotFoundComponent}
+];
