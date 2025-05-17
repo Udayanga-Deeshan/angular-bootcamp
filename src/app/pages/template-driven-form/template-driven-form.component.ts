38b5dae04e2d7325dafb692e636e39c3125a9c39
Udayanga-Deeshan <Udayangadilshan1789@gmail.com>
2025-05-17 22:54:38 +0530

    patch value method & reset form data

--- src/app/pages/template-driven-form/template-driven-form.component.ts
+++ src/app/pages/template-driven-form/template-driven-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { User } from '../../model/user';
 import { JsonPipe } from '@angular/common';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-template-driven-form',
@@ -16,6 +17,19 @@ export class TemplateDrivenFormComponent {
   onSubmit(userForm:NgForm){
       console.log(userForm.value);
       
+  }
+
+  setValues(userForm:NgForm){
+      let obj ={
+        firstName:"Teena",
+        lastName:"judith"
+      }
+
+      userForm.control.patchValue(obj)
+  }
+
+  resetData(userForm:NgForm){
+      userForm.reset()
   }
 
 }
